8ad42b15bb00ed0e0406519cd462e00f22461229
Kevin Ly <kevinly@alumni.ubc.ca>
2022-01-16 05:41:41 -0800

    Implemented UI ahead of backend integration

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def index():
+    if request.method == "POST":
+        course_code = request.form.getlist("course_code")
+        subject_code = request.form.get("subject_code")
+        average = request.form.get("average")
+        offered = request.form.getlist("offered")
+        print(course_code)
+        print(subject_code + " " + average)
+        print(offered)
     return render_template("index.html")
 
 if __name__ == '__main__':
